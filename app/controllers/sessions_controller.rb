6804b92f3b327b4ddf8f5f84715b67fb0fe9b345
irvin.sanchez <irvin.sanchez@nordstrom.com>
2024-01-24 11:12:35 -0600

    solution test

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    session = Session.new(session_params)
+
+    if session.save
+      render json: session, status: :created
+    else
+      render json: { errors: session.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:coach_hash_id, :client_hash_id, :start, :duration)
+  end
+end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,16 @@
 class Session < ApplicationRecord
+  validate :coach_schedule_not_overlapping
+
+  private
+
+  def coach_schedule_not_overlapping
+    if coach_hash_id.present? && start.present? && duration.present?
+      overlapping_sessions = Session.where(coach_hash_id: coach_hash_id)
+                                   .where.not(id: id) # Exclude the current session if updating
+                                   .where('start <= ? AND start >=  ?', start + duration.minutes, start)
+      if overlapping_sessions.exists?
+        errors.add(:base, 'Coach schedule is overlapping with another session')
+      end
+    end
+  end
 end
